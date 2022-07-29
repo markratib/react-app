fa70e2cee7c702e055001104f4abc381be5c5ce7
Mark Ratib <mark.ratib@revature.net>
2022-07-29 14:06:54 -0500

    added json db file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from "./Components/Header";
+import Tasks from "./Components/Tasks";
+import AddTask from "./Components/AddTask";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
 
+//rafce to create a stub of a functional component
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks (tasksFromServer);
+    }
+    getTasks();
+    
+  }, [])
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+
+    return data;
+  }
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+
+    return data;
+  }
+
+  //Add Task
+  const addTask = async (task) =>
+  {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {'content-type': 'application/json'} ,
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+    //Pre-JSON server approach
+    // console.log(task)
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = {id, ...task}
+    // setTasks([...tasks, newTask])
+
+  }
+
+  //Delete task
+  const deleteTask = async (id) => 
+  {
+    console.log('http://localhost:5000/tasks/' + id, { method: 'DELETE' })
+    await fetch('http://localhost:5000/tasks/' + id, { method: 'DELETE' })
+    setTasks(tasks.filter((task) => task.id !== id)) 
+  }
+
+  //Toggle reminder
+  const toggleReminder = async (id) =>
+  {
+    const taskToToggle = await fetchTask(id)
+    const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,
+    {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
+    setTasks(tasks.map((task) => 
+    task.id === id ? {...task, reminder: data.reminder} : task))
+    //Pre JSON server approach
+    // setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //can only return one parent element
+    <Router>
+      <div className='App container'>
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+
+        <Routes>
+          {/* <Route path='/about' component={About} /> */}
+          <Route path='/' exact element= {
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks'}
+            </>
+          }/>
+          <Route path='/about' element={ <About/> }/>
+        </Routes> 
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
+
+
+//class based componenet
+// class App extends React.Component
+// {
+//   render()
+//   {
+//     return <h1>Hello from a class!</h1>
+//   }
+// }
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+import { useLocation } from 'react-router-dom'
+
+// const Header = (props) => {
+//   return (
+//     <header>
+//         <h1 style={headingStyle}>{props.title}</h1>
+//     </header>
+//   )
+// }
+
+// const headingStyle =
+// {
+//     color: 'red', 
+//     backgroundColor: 'black'
+// }
+
+const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
+
+    return (
+        <header className='header'>
+            <h1>
+                {title}
+            </h1>
+            {location.pathname === '/' && <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>}
+        </header>
+    )
+}
+Header.defaultProps = 
+{
+    title: 'Task Tracker',
+}
+Header.propTypes = 
+{
+    title: PropTypes.string,
+}
+
+
+export default Header
